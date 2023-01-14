58ee276ee8008c16207214d8f83cb0da84ad8ea6
Dan <98132259+Dan090807@users.noreply.github.com>
2023-01-14 15:24:26 -0600

    Added Classes
    Added:
    - DriverProgram.
    - IRadio.
    - Radio.

---
+++ DriverProgram.java
@@ -0,0 +1,14 @@
+public class DriverProgram {
+
+    public static void main(String[] args){
+        IRadio miRadio = new Radio();
+        
+        if (miRadio.isOn()){
+            //Mostar el menu cuando el radio esta encendio
+        } else {
+            
+        }
+        
+    }
+    
+}
